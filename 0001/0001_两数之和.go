2161595f0b80afc00f698fa3e0a98b32c7c3c0e0
si.chen <si.chen@shopee.com>
2023-08-29 21:44:02 +0800

    0001_两数之和

--- 0001/0001_两数之和.go
+++ 0001/0001_两数之和.go
@@ -3,7 +3,16 @@ package twoSum
 // 2023-08-29 20:28:02
 // leetcode submit region begin(Prohibit modification and deletion)
 func twoSum(nums []int, target int) []int {
+	hashMap := make(map[int]int)
 
+	for idx, num := range nums {
+		if idx2, ok := hashMap[target-num]; ok {
+			return []int{idx2, idx}
+		}
+		hashMap[num] = idx
+	}
+
+	return nil
 }
 
 //leetcode submit region end(Prohibit modification and deletion)
