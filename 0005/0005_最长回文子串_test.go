c9c49d318384136d772108381347402f73156c12
si.chen <si.chen@shopee.com>
2023-09-11 20:19:25 +0800

    save

---
+++ 0005/0005_最长回文子串_test.go
@@ -0,0 +1,74 @@
+package longestPalindromicSubstring
+
+import (
+	"testing"
+)
+
+func Test_isPalindrome(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want bool
+	}{
+		// TODO: Add test cases.
+
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := isPalindrome(tt.args.s); got != tt.want {
+				t.Errorf("isPalindrome() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_longestPalindrome(t *testing.T) {
+	type args struct {
+		s string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		// TODO: Add test cases.
+		{
+			name: "111",
+			args: args{
+				s: "babd",
+			},
+			want: "bab",
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := longestPalindrome(tt.args.s); got != tt.want {
+				t.Errorf("longestPalindrome() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_maxNum(t *testing.T) {
+	type args struct {
+		x int
+		y int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := maxNum(tt.args.x, tt.args.y); got != tt.want {
+				t.Errorf("maxNum() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
